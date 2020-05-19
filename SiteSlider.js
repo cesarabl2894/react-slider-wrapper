09d4818eb0111bd2e6b871045ee2d68013952257
cesarabl2894 <cesar_bonel@unah.hn>
2020-05-19 08:28:16 -0600

    Initial commit

---
+++ SiteSlider.js
@@ -0,0 +1,40 @@
+import React, { Component, Fragment } from 'react';
+import Slider from 'react-slick';
+import MagicSliderDots from 'react-magic-slider-dots';
+import 'react-magic-slider-dots/dist/magic-dots.css';
+
+class SiteSlider extends Component {
+  state = {
+    activeSlide: 0,
+    slideCount: 0
+  };
+  render() {
+    const { children, settings } = this.props;
+    const sliderSettings = {
+      arrows: true,
+      dots: true,
+      afterChange: (current) => this.setState({ activeSlide: current }),
+      ...settings,
+      appendDots: dots => {
+        return (
+          <MagicSliderDots 
+            dots={dots} 
+            numDotsToShow={settings.numDots || 3} 
+            dotWidth={30} 
+          />
+        );
+      },
+    }
+    return (
+      <Fragment>
+        <Slider {...sliderSettings}>
+          {children}
+        </Slider>
+        { settings.showCounter ? <div>{ `${this.state.activeSlide + 1} / ${children.length}`}</div> : null }
+      </Fragment>
+    )
+  }
+}
+
+
+export default SiteSlider;
