bf82a6757a5b1f202dda14c111e3a3e02ea2ef11
cesarabl2894 <cesar_bonel@unah.hn>
2020-05-19 20:37:36 -0600

    Adding Custom Styles and fonts

--- src/SliderWrapper.js
+++ src/SliderWrapper.js
@@ -2,8 +2,9 @@ import React, { Component, Fragment } from 'react';
 import Slider from 'react-slick';
 import MagicSliderDots from 'react-magic-slider-dots';
 import 'react-magic-slider-dots/dist/magic-dots.css';
-import './css/slick.min.css';
-// import './css/theme.css';
+import './assets/css/slick.min.css';
+import './assets/css/theme.css';
+import PropTypes from 'prop-types';
 
 class SliderWrapper extends Component {
   constructor(props) {
@@ -16,7 +17,7 @@ class SliderWrapper extends Component {
   }
   
   render() {
-    const { children, settings } = this.props;
+    const { children, settings, showCounter } = this.props;
     const sliderSettings = {
       arrows: true,
       dots: true,
@@ -37,11 +38,19 @@ class SliderWrapper extends Component {
         <Slider {...sliderSettings}>
           {children}
         </Slider>
-        { settings.showCounter ? <div>{ `${this.state.activeSlide + 1} / ${children.length}`}</div> : null }
+        { showCounter ? <div>{ `${this.state.activeSlide + 1} / ${children.length}`}</div> : null }
       </Fragment>
     )
   }
 }
 
+SliderWrapper.defaultProps = {
+  showCounter: false
+}
+
+SliderWrapper.PropTypes = {
+  settings: PropTypes.object,
+  showCounter: PropTypes.bool
+}
 
 export default SliderWrapper;
