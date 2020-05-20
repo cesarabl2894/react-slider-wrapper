e33de95710d5f0460bac07e7d7c97241eaa692df
cesarabl2894 <cesar_bonel@unah.hn>
2020-05-19 19:13:40 -0600

    Edits to the webpack config

--- webpack.config.js
+++ webpack.config.js
@@ -1,40 +1,33 @@
-const webpack = require('webpack');
-const path = require('path');
+var path = require('path');
 
-const config = {
-  entry: './src/index.js',
-  output: {
-    path: path.resolve(__dirname, 'build'),
-    filename: 'index.js',
-    libraryTarget: 'commonjs2'
-  },
-  module: {
-    rules: [
-      {
-        test: /\.(js|jsx)$/,
-        exclude: /node_modules/,
-        use: {
-          loader: 'babel-loader',
-          options: {
-            presets: ['@babel/preset-env']
-          }
+module.exports = {
+    mode: 'production',
+    entry: './src/index.js',
+    output: {
+        path: path.resolve('lib'),
+        filename: 'index.js',
+        libraryTarget: 'commonjs2'
+    },
+    module: {
+      rules: [
+        {
+          test: /\.js?$/,
+          exclude: /(node_modules)/,
+          use: 'babel-loader'
+        },        
+        {
+          test: /\.css$/,
+          use: [
+            'style-loader',
+            'css-loader'
+          ]
         }
-      },
-      {
-        test: /\.css$/,
-        use: [
-          'style-loader',
-          'css-loader'
-        ]
-      }
-    ]
-  },
-  resolve: {
-    extensions: [
-      '.js',
-      '.jsx'
-    ]
-  }
-};
-
-module.exports = config;
+      ]
+    },
+    resolve: {
+      extensions: [
+        '.js',
+        '.jsx'
+      ]
+    }
+}
